da7dbeabed6e97c3df61abf4a3908a85a8564414
Moremoregreen <zzz2009j@gmail.com>
2018-07-24 02:05:28 +0800

    基本Lib加入 Layout建置

---
+++ app/src/main/java/com/moremoregreen/androidinstagramfilter/Utils/NonSwipeableViewPager.java
@@ -0,0 +1,59 @@
+package com.moremoregreen.androidinstagramfilter.Utils;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.Scroller;
+
+import java.lang.reflect.Field;
+
+public class NonSwipeableViewPager extends ViewPager {
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    public NonSwipeableViewPager(@NonNull Context context) {
+        super(context);
+        setMyScroller();
+    }
+
+    private void setMyScroller() {
+        try {
+            Class<?> viewpager = ViewPager.class;
+            Field scroller = viewpager.getDeclaredField("mScroller");
+            scroller.setAccessible(true);
+            scroller.set(this, new Myscroller(getContext()));
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public NonSwipeableViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        setMyScroller();
+    }
+
+    private class Myscroller extends Scroller {
+        public Myscroller(Context context) {
+            super(context, new DecelerateInterpolator());
+        }
+
+        @Override
+        public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+            super.startScroll(startX, startY, dx, dy, 400);
+        }
+    }
+}
